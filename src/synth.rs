c6e575ef8ccdefa8b573e5e161dc002c6f19153c
mitchmindtree <mitchell.nordine@gmail.com>
2015-04-26 18:22:10 +1000

    Use Sample method for buffer addition in Synth::audio_requested.

--- src/synth.rs
+++ src/synth.rs
@@ -332,7 +332,7 @@ impl<S> DspNode<S> for Synth where S: Sample {
         });
 
         let amp_multi = vol * normaliser;
-        //let vol_per_channel = [amp_multi; 2];
+        let vol_per_channel = vec![amp_multi; settings.channels as usize];
 
         // Request audio from each voice and sum them together.
         for voice in voices.iter_mut() {
@@ -342,10 +342,7 @@ impl<S> DspNode<S> for Synth where S: Sample {
                               duration,
                               loop_data_samples.as_ref(),
                               fade_data_samples.as_ref());
-            for (output_sample, sample) in output.iter_mut().zip(working.into_iter()) {
-                *output_sample = *output_sample + sample.mul_amp(amp_multi);
-            }
-            //Sample::add_buffers(output, &working, &vol_per_channel);
+            Sample::add_buffers(output, &working, &vol_per_channel);
         }
     }
 
