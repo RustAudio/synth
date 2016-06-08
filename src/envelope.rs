608d5225a401594f137515437f61cc55d4ebad52
mitchmindtree <mitchell.nordine@gmail.com>
2016-06-08 15:59:54 +1000

    Clean up std namespacing.

--- src/envelope.rs
+++ src/envelope.rs
@@ -1,6 +1,6 @@
 use envelope_lib;
 pub use envelope_lib::Envelope as Trait;
-use std::iter::FromIterator;
+use std;
 
 
 /// An alias to the type of point to be used for amp and freq interpolation.
@@ -12,13 +12,15 @@ pub struct Envelope {
     pub points: Vec<Point>,
 }
 
-impl FromIterator<Point> for Envelope {
-    fn from_iter<T>(iter: T) -> Self where T: IntoIterator<Item=Point> {
-        Envelope { points: iter.into_iter().collect(), }
+impl std::iter::FromIterator<Point> for Envelope {
+    fn from_iter<T>(iter: T) -> Self
+        where T: IntoIterator<Item=Point>
+    {
+        Envelope { points: iter.into_iter().collect() }
     }
 }
 
-impl ::std::convert::From<Vec<Point>> for Envelope {
+impl std::convert::From<Vec<Point>> for Envelope {
     fn from(points: Vec<Point>) -> Self {
         Envelope { points: points }
     }
@@ -28,7 +30,7 @@ impl<'a> Trait<'a> for Envelope {
     type X = f64;
     type Y = f64;
     type Point = Point;
-    type Points = ::std::slice::Iter<'a, Point>;
+    type Points = std::slice::Iter<'a, Point>;
     #[inline]
     fn points(&'a self) -> Self::Points { self.points.iter() }
 }
